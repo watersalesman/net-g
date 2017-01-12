b37707f4127772bcf67418fd0504adbbca7db847
watersalesman <rramos1295@gmail.com>
2017-01-12 10:51:28 -0500

    Used install command and removed systemctl calls

--- netg-install.sh
+++ netg-install.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-echo "Removing any present Net-G services..."
-systemctl stop netg.service
-systemctl disable netg.service
-cp netg.service /usr/lib/systemd/system/netg.service
-cp netg-pref.sh /usr/bin/netg-pref
-cp netg.py /usr/bin/netg
+echo 'g++ -o netg-d netg-d.cpp'
+g++ -o netg-d netg-d.cpp
 echo
-echo 'g++ -o /usr/bin/netg-d netg-d.cpp'
-g++ -o /usr/bin/netg-d netg-d.cpp
-echo
-chmod +x /usr/bin/netg-d
-chmod +x /usr/bin/netg-pref
-chmod +x /usr/bin/netg
-chmod 744 /usr/bin/netg-d
-chmod 744 /usr/bin/netg-pref
-chmod 744 /usr/bin/netg
+install -m 644 netg.service /usr/lib/systemd/system/netg.service
+install -m 744 netg-pref.sh /usr/bin/netg-pref
+install -m 744 netg.py /usr/bin/netg
+install -m 744 netg-d /usr/bin/netg-d
 echo
 echo 'Enable "netg.service" for daemon'
 echo 'or run "netg" for single use'
